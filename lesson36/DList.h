b5960c724dd4d2513183ab1d1f03572c9e5aeecd
Vanya Zinovchuk(Carpediem0000) <zinovchukdfuz2@gmail.com>
2023-11-23 23:08:59 +0200

    HM

--- lesson36/DList.h
+++ lesson36/DList.h
@@ -31,6 +31,20 @@ namespace DList {
 		List() {
 			head = tail = nullptr;
 		}
+		~List() {
+			Node<U>* p = head;
+			if (head != nullptr)
+			{
+				while (p != nullptr)
+				{
+					Node<U>* nextNode = p->next;
+					delete p;
+					p = nextNode;
+				}
+			}
+			head = nullptr;
+			tail = nullptr;
+		}
 
 		void pushBack(U value) {
 			Node<U>* el = new Node<U>(value);
@@ -60,7 +74,7 @@ namespace DList {
 			}
 		}
 
-		void insert(U after, U value) {
+		void insertAfter(U after, U value) {
 			Node<U>* el = new Node<U>(value);
 			if (head == nullptr)
 			{
@@ -87,6 +101,81 @@ namespace DList {
 					tail = el;
 				}
 			}
+		}
+
+		void insertBefore(U before, U value) {
+			Node<U>* el = new Node<U>(value);
+			if (head == nullptr)
+			{
+				head = tail = el;
+			}
+			else
+			{
+				Node<U>* p = head;
+				while (p != nullptr)
+				{
+					if (p->info == before)
+					{
+						el->next = p;
+						el->prev = p->prev;
+						p->next = p->next->next;
+						p->prev = el;
+						return;
+					}
+					p = p->next;
+				}
+				if (p == nullptr)
+				{
+					tail->next = el;
+					el->prev = tail;
+					tail = el;
+				}
+			}
+		}
+
+		void erase(U value) {
+			if (head == nullptr)
+			{
+				cout << "List is empty!\n";
+			}
+			else
+			{
+				Node<U>* p = head;
+				while (p != nullptr)
+				{
+					if (p->info == value) {
+						if (p->prev == nullptr) {
+							head = p->next;
+						}
+						else {
+							p->prev->next = p->next;
+							p->next->prev = p->prev;
+						}
+						delete p;
+						return;
+					}
+					p = p->next;
+				}
+			}
+		}
+
+		DNode<U>* search(U value) {
+			if (head == nullptr)
+			{
+				cout << "List is empty!\n";
+			}
+			else
+			{
+				Node<U>* p = head;
+				while (p != nullptr)
+				{
+					if (p->info == value) {
+						return p;
+					}
+					p = p->next;
+				}
+			}
+			return nullptr;
 		}
 
 		void show()const {
